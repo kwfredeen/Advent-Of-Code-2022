caecb2cd7b7db58a2d3ef1eaf8927578891a759b
Ki Fredeen <kwfredeen@gmail.com>
2022-12-01 23:56:11 -0900

    Day 2 Part 1 Solutions

---
+++ Day 2 Part 1/Day2Helpers.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_Part_1
+{
+    public class Day2Helpers
+    {
+        //opponent's plays
+        public const string opponentRock = "A";
+        public const string opponentPaper = "B";
+        public const string opponentScissors = "C";
+
+        //your plays
+        public const string youRock = "X";
+        public const string youPaper = "Y";
+        public const string youScissors = "Z";
+
+        /// <summary>
+        /// Calculates a score for one round of elvish Rock Paper Scissors based pn rules outlined at https://adventofcode.com/2022/day/2
+        /// </summary>
+        /// <param name="opponentPlay">Shape your opponent plays. must be A, B, or C for Rock, Paper, and Scissors respectively</param>
+        /// <param name="yourPlay">Shape you play. must be X, Y, or Z for Rock, Paper, and Scissors respectively</param>
+        /// <returns>score of the round</returns>
+        /// <exception cref="ArgumentException"> if either player's plays are not A,B,C or X,Y,Z</exception>
+        public static int RoundScore(string opponentPlay, string yourPlay)
+        {
+            int score = 0;
+
+            //add the score for the shape you played
+            score += yourPlay switch
+            {
+                youRock => 1,
+                youPaper => 2,
+                youScissors => 3,
+                _ => throw new ArgumentException("your play must be X, Y, or Z"),
+            };
+
+            //add score from match result
+            if (yourPlay == youRock)
+            {
+                score += opponentPlay switch
+                {
+                    opponentRock => 3, //draw
+                    opponentScissors => 6, //win
+                    opponentPaper => 0, //loss
+                    _ => throw new ArgumentException("opponent play must be A, B, or C")
+                };
+            }
+            else if (yourPlay == youPaper)
+            {
+                score += opponentPlay switch
+                {
+                    opponentPaper => 3, //draw
+                    opponentRock => 6, //win
+                    opponentScissors => 0, //loss
+                    _ => throw new ArgumentException("opponent play must be A, B, or C")
+                };
+            } else if (yourPlay == youScissors)
+            {
+                score += opponentPlay switch
+                {
+                    opponentScissors => 3, //draw
+                    opponentPaper => 6, //win
+                    opponentRock => 0, //loss
+                    _ => throw new ArgumentException("opponent play must be A, B, or C")
+                };
+            }
+
+            return score;
+        }
+    }
+}
---
+++ Day 2 Part 1/Day2Part1.cs
@@ -0,0 +1,35 @@
+﻿using Day_2_Part_1;
+
+int totalScore = 0;
+
+while (true)
+{
+    string? line = Console.ReadLine();
+
+    if (line is null || line == "q" || line == "")
+    {
+        //quit
+        Console.WriteLine($"Score: {totalScore}");
+        break;
+    } else if (line == "r")
+    {
+        //reset
+        Console.WriteLine($"Score: {totalScore}");
+        totalScore = 0;
+        continue;
+    }
+
+    string[] splitLine = line.Split(" ", 2);
+    string opponent = splitLine[0];
+    string you = splitLine[1];
+
+    totalScore += Day2Helpers.RoundScore(opponent, you);
+}
+
+Console.WriteLine("Goodbye. Press enter to exit");
+//pause to let user read output
+Console.ReadLine();
+
+/* Potential improvements:
+ *  - these quit commands feel clunky and could be improved.
+ */  
