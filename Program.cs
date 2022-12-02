4524657f88cc791fb71d5f7b93fca8d0ccc2a475
Ki Fredeen <kwfredeen@gmail.com>
2022-12-01 21:56:58 -0900

    Day 1 Part 1 Solution

--- Program.cs
+++ Program.cs
@@ -1,1 +1,34 @@
-﻿List<int> elvesCalories = new();
+﻿//https://adventofcode.com/2022/day/1
+
+//most calories held by any elf
+int mostCalories = 0;
+
+//the current elf's calories
+int elfSum = 0;
+//read console input
+while (true)
+{
+    string? line = Console.ReadLine();
+
+    //check if this elf has no more snacks (or the next elf has started recording)
+    if(line is null || line == "")
+    {
+        //check if this elf has the most calories
+        if (elfSum > mostCalories) mostCalories = elfSum;
+
+        //report which elf has the most calories
+        Console.WriteLine($"Most Calories: {mostCalories}");
+
+        //reset elf sum and move onto the next elf with food
+        elfSum = 0;
+        continue;
+    }
+
+    //elf still has snacks, add calories of next snack
+    elfSum += int.Parse(line);
+}
+
+/* Potential improvements:
+ *  - Last line needs user to press enter twice manually. This could be fixed.
+ *  - Add built in commands to reset, quit
+ */
