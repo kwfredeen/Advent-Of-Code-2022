75843111de7a10591b5bcc28fb3713644b67297a
Ki Fredeen <kwfredeen@gmail.com>
2022-12-05 19:00:48 -0900

    Day 4 Part 1 solutions

---
+++ Day 4 Part 1/Day4Helpers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_4_Part_1
+{
+    public class Day4Helpers
+    {
+        /// <summary>
+        /// Parses a section range string into a section range array.
+        /// </summary>
+        /// <param name="section">section range string to parse</param>
+        /// <returns>section range array, where [0] is the start and [1] is the end of the section range</returns>
+        /// <exception cref="ArgumentException">when section string has more than one hyphen</exception>
+        public static int[] ParseSection(string section)
+        {
+            int[] startEnd = section.Split('-').Select(s => int.Parse(s)).ToArray();
+
+            if (startEnd.Length != 2) throw new ArgumentException("section must have exactly two numbers separated by one hyphen!");
+
+            return startEnd;
+        }
+
+        /// <summary>
+        /// Checks if two section range arrays are fully overlapped
+        /// </summary>
+        /// <param name="range1">first section range array</param>
+        /// <param name="range2">second section range array</param>
+        /// <returns>true if either section range is completely covered by the other</returns>
+        public static bool CheckFullSectionOverlap(int[] range1, int[] range2)
+        {
+            bool oneInsideTwo = range1[0] >= range2[0] && range1[1] <= range2[1];
+
+            //check the other direction
+            bool twoInsideOne = range1[0] <= range2[0] && range1[1] >= range2[1];
+
+            return oneInsideTwo || twoInsideOne;
+        }
+    }
+}
---
+++ Day 4 Part 1/Day4Part1.cs
@@ -0,0 +1,29 @@
+﻿using Day_4_Part_1;
+
+int completeoverlappedCount = 0;
+
+while (true)
+{
+    string? line = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(line))
+    {
+        Console.WriteLine($"Count: {completeoverlappedCount}");
+        continue;
+    }
+
+    //split the two ranges
+    string[] sectionRanges = line.Split(',');
+
+    string rangeString1 = sectionRanges[0];
+    string rangeString2 = sectionRanges[1];
+
+    //parse section range strings into section range arrays
+    int[] rangeArray1 = Day4Helpers.ParseSection(rangeString1);
+    int[] rangeArray2 = Day4Helpers.ParseSection(rangeString2);
+
+    if (Day4Helpers.CheckFullSectionOverlap(rangeArray1, rangeArray2))
+    {
+        completeoverlappedCount++;
+    }
+}
