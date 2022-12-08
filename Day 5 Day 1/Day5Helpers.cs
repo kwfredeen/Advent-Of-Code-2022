00c7cec26f018bfcc6b62c7e63e0691fa7b11b08
Ki Fredeen <kwfredeen@gmail.com>
2022-12-07 22:36:46 -0900

    Finished Day 5 solution

--- Day 5 Day 1/Day5Helpers.cs
+++ Day 5 Day 1/Day5Helpers.cs
@@ -62,5 +62,101 @@ namespace Day_5_Part_1
 
             return levelList;
         }
+
+        /// <summary>
+        /// Removes empty crates (spaces) from stacks
+        /// </summary>
+        /// <param name="stacks">stacks to remove spaces from</param>
+        public static void StripEmptySpace(List<Queue<char>> stacks)
+        {
+            foreach(Queue<char> stack in stacks)
+            {
+                while(stack.Peek() == ' ')
+                {
+                    //remove the space
+                    _ = stack.Dequeue();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Executes a move command on stacks of crates.
+        /// </summary>
+        /// <param name="stacks">stacks to move</param>
+        /// <param name="move">move command</param>
+        public static void ExecuteMove(List<Stack<char>> stacks, string move)
+        {
+            var command = ParseMoveCommand(move);
+
+            for(int i = 0; i < command.cratesToMove; i++)
+            {
+                Stack<char> destinationStack = stacks[command.to - 1];
+                Stack<char> sourceStack = stacks[command.from - 1];
+                destinationStack.Push(sourceStack.Pop());
+            }
+        }
+
+        /// <summary>
+        /// Parses a move command into number of crates to move, and source and destination stacks
+        /// </summary>
+        /// <param name="command">crane move command to parse</param>
+        /// <returns>Value tuple with number of crates to move, and to and from stack numbers</returns>
+        private static (int cratesToMove, int from, int to) ParseMoveCommand(string command)
+        {
+            List<int> commandNumbers = AllNumbersinString(command);
+            
+            return (commandNumbers[0], commandNumbers[1], commandNumbers[2]);
+        }
+
+        /// <summary>
+        /// Converts Queue representation of stacks (for construction) to Stacks representation (for moving)
+        /// </summary>
+        /// <param name="stackQueues">stacks as a list of queues</param>
+        /// <returns>stacks as a list of stacks</returns>
+        public static List<Stack<char>> StacksFromQueues(List<Queue<char>> stackQueues)
+        {
+            List<Stack<char>> stackStacks = new();
+
+            foreach(Queue<char> stack in stackQueues)
+            {
+                stackStacks.Add(new Stack<char>(stack.Reverse()));
+            }
+
+            return stackStacks;
+        }
+
+        /// <summary>
+        /// Parses every number present in a string
+        /// </summary>
+        /// <param name="str">string to extract numbers from, where all numbers are positive integers</param>
+        /// <returns>List of positive ints found in the string</returns>
+        private static List<int> AllNumbersinString(string str)
+        {
+            List<int> allnumbers = new();
+
+            bool inNumber = false;
+            string currentNumber = "";
+            foreach(char c in str)
+            {
+                if (Char.IsDigit(c))
+                {
+                    inNumber = true;
+                    currentNumber += c;
+                } else if (inNumber)
+                {
+                    allnumbers.Add(int.Parse(currentNumber));
+                    inNumber = false;
+                    currentNumber = "";
+                }
+            }
+
+            //in case last char was part of a number
+            if (inNumber)
+            {
+                allnumbers.Add(int.Parse(currentNumber));
+            }
+
+            return allnumbers;
+        }
     }
 }
