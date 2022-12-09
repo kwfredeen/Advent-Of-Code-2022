6a85e75ebdd3ab66cd8f5862a561e9c7bb4c33e8
Ki Fredeen <kwfredeen@gmail.com>
2022-12-08 21:22:05 -0900

    Day 6 Part 2 solution

---
+++ Day 6 Part 2/Day6Part2.cs
@@ -0,0 +1,19 @@
+﻿while (true)
+{
+    Console.WriteLine("Input a datastream");
+    string stream = Console.ReadLine()!;
+
+    //slide 4 char window down the line
+    for (int i = 0; i < stream.Length - 14; i++)
+    {
+        HashSet<char> window = stream.Skip(i).Take(14).ToHashSet();
+
+        if (window.Count != 14) continue;
+        Console.WriteLine(i + 14);
+        break;
+    }
+}
+
+/* Potential Improvements
+ *  - Move sliding window code to own function, with generalized input for a window of any size
+ */
