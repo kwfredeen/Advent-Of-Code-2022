f0b932005b5795a203157dd2d276fb149355e847
Ki Fredeen <kwfredeen@gmail.com>
2022-12-08 21:13:05 -0900

    Day 6 Part 1 solution

---
+++ Day 6 Part 1/Day6Part1.cs
@@ -0,0 +1,15 @@
+﻿while (true)
+{
+    Console.WriteLine("Input a datastream");
+    string stream = Console.ReadLine()!;
+
+    //slide 4 char window down the line
+    for (int i = 0; i < stream.Length - 4; i++)
+    {
+        HashSet<char> window = stream.Skip(i).Take(4).ToHashSet();
+
+        if (window.Count != 4) continue;
+        Console.WriteLine(i + 4);
+        break;
+    }
+}
